def2bdc7cbda7e1cd0d4b836aa5525b4409aebda
autopp <autopp.inc@gmail.com>
2017-03-05 22:16:57 +0900

    add Round

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,4 @@
 class Tournament < ApplicationRecord
   has_many :players
+  has_many :rounds
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305124345) do
+ActiveRecord::Schema.define(version: 20170305131524) do
 
   create_table "players", force: :cascade do |t|
     t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rounds", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
