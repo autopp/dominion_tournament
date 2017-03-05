435a9688df36d277f714de9d2a4deac8010e119b
autopp <autopp.inc@gmail.com>
2017-03-05 21:49:44 +0900

    add Player

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305115556) do
+ActiveRecord::Schema.define(version: 20170305124345) do
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tournaments", force: :cascade do |t|
     t.datetime "created_at", null: false
