2109df9df136c5d2c174487d35cc76d0f1da6bed
autopp <autopp.inc@gmail.com>
2017-03-05 23:50:19 +0900

    skip generation of view, routing, helper spec

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,15 @@ Bundler.require(*Rails.groups)
 
 module DominionTournament
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm :active_record
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       routing_specs: false,
+                       helper_specs: false,
+                       integration_tool: false
+    end
   end
 end
