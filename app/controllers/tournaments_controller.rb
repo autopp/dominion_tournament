caa4161cab5f149f18b2c55d025d34cd7dbab9e8
autopp <autopp.inc@gmail.com>
2017-03-13 22:04:16 +0900

    impl tournament create

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -7,6 +7,16 @@ class TournamentsController < ApplicationController
   end
 
   def create
+    players = params[:players].each_line.map(&:strip).reject(&:blank?)
+    t = Tournament.new
+    ActiveRecord::Base.transaction do
+      t.save!
+      players.each do |name|
+        t.players.create!(name: name)
+      end
+    end
+
+    redirect_to tournament_path(id: t.id)
   end
 
   def show
