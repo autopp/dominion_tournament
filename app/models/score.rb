71a8c97436ef332f228a44e9eda90dc603259320
autopp <autopp.inc@gmail.com>
2017-03-17 00:00:10 +0900

    impl Score#vp & Score#<=>

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,24 @@
 class Score < ApplicationRecord
   belongs_to :player
   belongs_to :table
+
+  include Comparable
+
+  def vp
+    Rational(vp_numerator, vp_denominator)
+  end
+
+  def <=>(other)
+    raise TypeError, "operand is not a Score (#{other.inspect})" unless other.is_a?(Score)
+
+    diff = vp - other.vp
+
+    return diff unless diff.zero?
+
+    if has_extra_turn? == other.has_extra_turn?
+      0
+    else
+      has_extra_turn? ? -1 : 1
+    end
+  end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Score, type: :model do
+  let(:score) { described_class.new(vp_numerator: 42, vp_denominator: 1) }
+
+  describe '#<=>' do
+    subject { score <=> other }
+
+    context 'when other is smaller' do
+      let(:other) { described_class.new(vp_numerator: 41, vp_denominator: 1) }
+
+      it { is_expected.to be > 0 }
+    end
+
+    context 'when other is greater' do
+      let(:other) { described_class.new(vp_numerator: 43, vp_denominator: 1) }
+
+      it { is_expected.to be < 0 }
+    end
+
+    context 'when other is equal' do
+      context 'and when other dose not has extra turn' do
+        let(:other) { described_class.new(vp_numerator: 42, vp_denominator: 1) }
+
+        it { is_expected.to be_zero }
+      end
+
+      context 'and when other dose not has extra turn' do
+        let(:other) do
+          described_class.new(vp_numerator: 42, vp_denominator: 1, has_extra_turn: true)
+        end
+
+        it { is_expected.to be > 0 }
+      end
+    end
+  end
 end
