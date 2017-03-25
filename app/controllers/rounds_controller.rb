eb91f4faa7a61b6ff683833644831f64ca8c782d
autopp <autopp.inc@gmail.com>
2017-03-25 11:11:48 +0900

    extract method to reduce ABC size

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,13 +2,7 @@ class RoundsController < ApplicationController
   def create
     @tournament = Tournament.find(params[:tournament_id])
     ActiveRecord::Base.transaction do
-      @round = @tournament.rounds.create!(number: @tournament.rounds.count + 1)
-      @tournament.matchings.each.with_index(1) do |players, i|
-        table = @round.tables.create!(number: i)
-        players.each do |player|
-          table.scores.create!(player: player)
-        end
-      end
+      @round = create_round(@tournament)
     end
 
     redirect_to edit_tournament_round_path(tournament_id: @tournament.id, id: @round.number)
@@ -25,5 +19,19 @@ class RoundsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def create_round(tournament)
+    round = tournament.rounds.create!(number: tournament.rounds.count + 1)
+    tournament.matchings.each.with_index(1) do |players, i|
+      table = round.tables.create!(number: i)
+      players.each do |player|
+        table.scores.create!(player: player)
+      end
+    end
+
+    round
   end
 end
