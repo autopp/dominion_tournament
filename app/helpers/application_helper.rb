7dd903053c4c5b77915f196b053be225530342eb
autopp <autopp.inc@gmail.com>
2017-03-27 17:51:14 +0900

    fix view of 3 players table's TP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def format_point(point)
-    point.is_a?(Integer) || point.denominator == 1 ? point.numerator : format('%.2f', point.to_f)
+    point.is_a?(Integer) || point.denominator == 1 ? point.numerator : format('%.4g', point.to_f)
   end
 end
