a4611baae1e47e5b374683a26b8a5385e8498bd4
autopp <autopp.inc@gmail.com>
2017-09-23 17:48:09 +0900

    add check function about authority

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  AUTH_VALUES = { 'guest' => 0, 'staff' => 1, 'admin' => 2 }.freeze
+
   def render_with_errors(*args, errors:)
     @errors = errors
     render(*args)
+  end
+
+  def check_auth(auth, fall_back)
+    authorized = Rails.config.authority >= auth
+    render_with_errors(fall_back, 'Not permitted operation') unless authorized
+    authorized
   end
 end
