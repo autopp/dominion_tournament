3fc9fe0a5f7935a616f91c47b52269abc133526c
autopp <autopp.inc@gmail.com>
2017-09-25 09:57:19 +0900

    add feature spec for finish round

--- spec/features/edit_round_page_spec.rb
+++ spec/features/edit_round_page_spec.rb
@@ -1,12 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'round editing page', type: :feature do
-  before { create(:tournament_with_ongoing_third_rounds) }
+  before do
+    @tournament = create(:tournament_with_ongoing_third_rounds)
+  end
 
   context 'when round is already finished' do
     it 'redirect to show page' do
       visit edit_tournament_round_path(tournament_id: 1, id: 2)
       expect(current_path).to eq(tournament_round_path(tournament_id: 1, id: 2))
+    end
+  end
+
+  context 'when authority level is not "admin"' do
+    include_context 'authority level is "staff"'
+
+    scenario 'click finish' do
+      visit edit_tournament_round_path(tournament_id: 1, id: 3)
+
+      click_on 'Finish'
+
+      expect(current_path).to eq(tournament_round_path(tournament_id: 1, id: 3))
+      expect(@tournament.finished_count).to eq(2)
+      expect(page).to have_css('div', id: 'error_explanation')
     end
   end
 end
