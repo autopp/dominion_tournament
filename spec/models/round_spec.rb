06abc7b72b2710b01b2545863e1d8cf031a0105c
autopp <autopp.inc@gmail.com>
2018-12-04 09:50:25 +0900

    test: add test of Round#finished?

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Round, type: :model do
+describe Round, type: :model do
+  describe 'finished?' do
+    subject { round.finished? }
+
+    context 'when own number equals to or greater than finished count of tournament' do
+      let(:round) do
+        t = create(:tournament_with_finished_two_rounds)
+        t.rounds.first
+      end
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when own number is less than finished count of tournament' do
+      let(:round) do
+        t = create(:tournament_with_ongoing_third_rounds)
+        t.rounds.last
+      end
+
+      it { is_expected.to be_falsey }
+    end
+  end
 end
