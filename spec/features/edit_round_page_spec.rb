98791dbf2d43f5ec192c06e06389ad4878095746
autopp <autopp.inc@gmail.com>
2018-12-31 15:45:25 +0900

    refactor: use let for ids

--- spec/features/edit_round_page_spec.rb
+++ spec/features/edit_round_page_spec.rb
@@ -5,22 +5,28 @@ RSpec.describe 'round editing page', type: :feature do
     @tournament = create(:tournament_with_ongoing_third_rounds)
   end
 
+  let(:page_path) { edit_tournament_round_path(tournament_id: @tournament.id, id: round_number) }
+
   context 'when round is already finished' do
+    let(:round_number) { 2 }
+
     it 'redirect to show page' do
-      visit edit_tournament_round_path(tournament_id: 1, id: 2)
-      expect(current_path).to eq(tournament_round_path(tournament_id: 1, id: 2))
+      visit page_path
+      expect(current_path).to eq(tournament_round_path(tournament_id: @tournament.id, id: round_number))
     end
   end
 
   context 'when authority level is not "admin"' do
     include_context 'authority level is "staff"'
 
+    let(:round_number) { 3 }
+
     scenario 'click finish' do
-      visit edit_tournament_round_path(tournament_id: 1, id: 3)
+      visit page_path
 
       click_on 'Finish'
 
-      expect(current_path).to eq(tournament_round_path(tournament_id: 1, id: 3))
+      expect(current_path).to eq(tournament_round_path(tournament_id: @tournament.id, id: round_number))
       expect(@tournament.finished_count).to eq(2)
       expect(page).to have_css('div', id: 'error_explanation')
     end
