d2ba42a1d3df22b963d2cc56d1bf8d6bb376a323
autopp <autopp.inc@gmail.com>
2019-01-12 23:01:40 +0900

    feat: add RoundEntity

---
+++ app/models/round_entity.rb
@@ -0,0 +1,45 @@
+class RoundEntity
+  include ActiveModel::Model
+
+  attr_accessor :tournament, :number, :tables
+
+  define_model_callbacks :initialize
+
+  def initialize(*args)
+    run_callbacks :initialize do
+      super
+    end
+  end
+
+  after_initialize do
+    @tables ||= (1..((tournament.players.count + 3) / 4)).map do |i|
+      Table.new(tournament: tournament, round_number: number, number: i)
+    end
+  end
+
+  def finished?
+    tournament.finished_count >= number
+  end
+
+  def finish!
+    ActiveRecord::Base.transaction do
+      tables.each(&:finish!)
+      tournament.finished_count += 1
+      tournament.save!
+    end
+  end
+
+  def not_completed_tables
+    tables.reject do |table|
+      table.scores.all?(&:complete?)
+    end
+  end
+
+  def rollback!
+    tables.each do |table|
+      table.scores.each do |score|
+        score.update!(tp_numerator: nil, tp_denominator: nil, rank: nil)
+      end
+    end
+  end
+end
---
+++ spec/models/round_entity_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe RoundEntity, type: :model do
+  describe 'finished?' do
+    subject { round.finished? }
+
+    context 'when own number equals to or greater than finished count of tournament' do
+      let(:round) do
+        described_class.new(tournament: Tournament.new(id: 1, finished_count: 2), number: 2)
+      end
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when own number is less than finished count of tournament' do
+      let(:round) do
+        described_class.new(tournament: Tournament.new(id: 1, finished_count: 2), number: 3)
+      end
+
+      it { is_expected.to be_falsey }
+    end
+  end
+end
