312c6f98f36e42cafb2c895e6b6ae0fad7f9791a
autopp <autopp.inc@gmail.com>
2019-01-14 16:24:00 +0900

    refactor: use RoundEntity instead of Round in RoundsController

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,7 +1,7 @@
 class RoundsController < ApplicationController
   before_action only: %i[show edit list update] do
-    @round = Round.find_by(tournament_id: params[:tournament_id], number: params[:id])
-    @tournament = @round.tournament
+    @tournament = Tournament.find(params[:tournament_id])
+    @round = RoundEntity.new(tournament: @tournament, number: params[:id].to_i)
     @tables = @round.tables
   end
 
