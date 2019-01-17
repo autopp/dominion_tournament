712ed4cbf136ef5bf01bdb98206930c7bc91c8b6
autopp <autopp.inc@gmail.com>
2019-01-17 22:40:24 +0900

    refactor: use simple consturctor instead of factory

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -6,8 +6,7 @@ describe Round, type: :model do
 
     context 'when own number equals to or greater than finished count of tournament' do
       let(:round) do
-        t = create(:tournament_with_finished_two_rounds)
-        t.rounds.first
+        described_class.new(tournament: Tournament.new(id: 1, finished_count: 2), number: 2)
       end
 
       it { is_expected.to be_truthy }
@@ -15,8 +14,7 @@ describe Round, type: :model do
 
     context 'when own number is less than finished count of tournament' do
       let(:round) do
-        t = create(:tournament_with_ongoing_third_rounds)
-        t.rounds.last
+        described_class.new(tournament: Tournament.new(id: 1, finished_count: 2), number: 3)
       end
 
       it { is_expected.to be_falsey }
